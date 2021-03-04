36d442b8f07bf6c37ae7d9a2e286340aff3e7f48
AZC <albertzcheng@gmail.com>
2021-03-03 21:08:48 -0800

    first commit

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
-        <span className="navbar-brand">Albert Cheng</span>
+        <span className="navbar-brand">Albert Z. Cheng</span>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
